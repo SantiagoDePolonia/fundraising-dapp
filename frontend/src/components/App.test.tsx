b2e621664b35817e104ea81ecc6e6f3b10a69afd
Jakub A. W <jakubwasek@gmail.com>
2023-01-30 12:06:13 +0100

    Remove redundant files

--- frontend/src/components/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './MainContainer';
-
-test('show connect wallet button', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/connect wallet/i);
-  expect(linkElement).toBeInTheDocument();
-});
